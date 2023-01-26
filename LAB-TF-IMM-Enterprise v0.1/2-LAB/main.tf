c7771f260d16cf4d616806fb642a2a57d6a6a7f2
bywhite <bywhite@cisco.com>
2023-01-26 12:19:23 -0600

    Updated Comments

--- LAB-TF-IMM-Enterprise v0.1/2-LAB/main.tf
+++ LAB-TF-IMM-Enterprise v0.1/2-LAB/main.tf
@@ -10,11 +10,8 @@
 # ------------------------------------------------------------------------------------------
 
 
-# Define Terraform Providers required for our main module
-
 terraform {
 
-    #Setting required version "=" to eliminate any future adverse behavior without testing first
     required_providers {
         intersight = {
             source = "CiscoDevNet/intersight"
@@ -28,9 +25,6 @@ provider "intersight" {
     secretkey = var.secretkey
     endpoint = "https://intersight.com"
 }
-
-#The target Intersight Organization should be created manually in Intersight
-# Example Use:  org_moid = data.intersight_organization_organization.my_org.id
 
 data "intersight_organization_organization" "my_org" {
     name = "default"
@@ -46,20 +40,11 @@ locals {
 
 
 # ===================== Define Input Variables  ==========================================
-# Define Input Variables
-# Usage from CLI:  terraform plan -var "apikey=<my-key>" -var "secretkey=<pem-key>"
-# Variables can be set in TFCB "Variables" section of the Workspace
-# Variables can be set with environment variables (MAC):  export TF_VAR_apikey=<my-api-key>
-
-# MAC CLI:        export TF_VAR_apikey=<my-api-key>
 variable "apikey" {
   description = "API key ID for Intersight account"
   type        = string
 }
 
-
-# MAC:    export TF_VAR_secretkey=`cat ~/Downloads/SecretKey.txt` 
-#                          Note:  ^  the two backticks above are not the same as single quotes ` vs '
 variable "secretkey" {
   description = "secret key for Intersight API vsn 2"
   type        = string
@@ -68,7 +53,6 @@ variable "secretkey" {
 
 
 # ===================== Define Output Variables  =========================================
-# Define Output Variables
 output "my_org" {
   value       = data.intersight_organization_organization.my_org.name
   description = "My Intersight Organization Name"
@@ -83,10 +67,6 @@ output "my_org" {
 # Chassis Profile
 # -----------------------------------------------------------------------------
 #   Reference: https://registry.terraform.io/providers/CiscoDevNet/intersight/latest/docs/resources/chassis_profile
-# ** Set policy alias (resource's instance name) to "chassis_9508_profile"
-# ** Set "action" and "control_action" parameters to "No-op"
-# ** Set "error_state" parameter to ""
-# ** Set the orgainization "moid" equal to your local organization variable (local.org_moid)
 
 resource "intersight_chassis_profile" "chassis_9508_profile" {
   name            = "chassis_profile1"
